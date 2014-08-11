3bea79da8f2d91409f77da6108ddbbfbf16099f4
Willy Collins <wcurtiscollins@gmail.com>
2014-08-10 21:25:37 -0400

    first

---
+++ chores/urls.py
@@ -0,0 +1,15 @@
+from chores import views
+
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'chores.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^', include(views.index)),
+    url(r'^admin/', include(admin.site.urls)),
+)
