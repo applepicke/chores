0e16990ed051446baf71a435a954ea884ceb3302
Willy Collins <wcurtiscollins@gmail.com>
2014-08-27 21:28:56 -0400

    Add fabric script

---
+++ fabfile.py
@@ -0,0 +1,10 @@
+from fabric.api import *
+
+@hosts('applepicke@chores.willyc.me')
+def production():
+  code_dir = '/home/applepicke/webapps/chores/chores/'
+  python = '/home/applepicke/webapps/chores/env/default/bin/python'
+  with cd(code_dir):
+    run("git pull")
+    run('%s manage.py syncdb' % python)
+    run('touch chores/wsgi.py')
