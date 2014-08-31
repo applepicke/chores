e00f4bf16334012f223dccfbbe42b14b38ab0e94
Willy Collins <wcurtiscollins@gmail.com>
2014-08-31 12:11:44 -0400

    fix broken admin

--- chores/urls.py
+++ chores/urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
   url(r'^logout/$', 'chores.views.logout_view', name='logout'),
   url(r'^admin/', include(admin.site.urls)),
   url(r'^houses/$', 'chores.views.index', name='houses'),
-  url(r'^house/(?P<id>\d+)/$', 'chores/views.index', name='house'),
+  url(r'^house/(?P<id>\d+)/$', 'chores.views.index', name='house'),
   url(r'^$', 'chores.views.index', name='index'),
 ) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
