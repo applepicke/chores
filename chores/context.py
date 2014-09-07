c9c975861621f92d1d84a522eef0d24f890bbfc3
Willy Collins <wcurtiscollins@gmail.com>
2014-09-06 21:39:33 -0400

    more fix

--- chores/context.py
+++ chores/context.py
@@ -4,7 +4,7 @@ def context(request, extra={}):
   ctx = {
     'APP_ID': settings.APP_ID,
     'user': request.user,
-    'house': request.app_user.house
+    'house': request.app_user.house if request.app_user else None,
   }
 
   return dict(ctx.items() + extra.items())
