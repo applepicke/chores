b7c4d62343d159debef007f929722392e5019a2c
calvin <calvin@calvin-VirtualBox.(none)>
2014-09-08 22:08:40 -0400

    tests now work...fixture will need to be updated as models change

--- chores/tests/views_tests.py
+++ chores/tests/views_tests.py
@@ -1,8 +1,33 @@
 from django.test import TestCase
+from chores import views
+import json
+import facebook
+import smtplib
 
-class BasicTest(TestCase):
+from django import http
+from django.conf import settings
+from django.contrib.auth import models as auth_models, login, logout, authenticate
+from django.shortcuts import render_to_response
+from django.core.urlresolvers import reverse
+from django.contrib.auth.decorators import login_required
+
+from chores.models import User, House, Chore
+from chores.utils import Facebook, untokenize
+from chores.context import context
+from chores.users.invitations import Invitation
+
+class ViewTest(TestCase):
+  fixtures = ['chores_views_testdata.json']
 
   def test_index(self):
-    resp = self.client.get('/')
+    resp = self.client.get(reverse('index'))
     self.assertEqual(resp.status_code, 200)
     
+  def test_login_view(self):
+    resp = self.client.get(reverse('login'))
+    self.assertEqual(resp.status_code, 200)
+
+  def test_api_house(self):
+    resp = self.client.get(reverse('api_house'))
+    self.assertEqual(resp.status_code, 200)
+    self.assertTrue('butt' in resp.context)
