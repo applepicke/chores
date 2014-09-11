24f7dcb58db1a4d6e8f3d8f50ec0230dc298a9d0
calvin <calvin@calvin-VirtualBox.(none)>
2014-09-11 19:47:04 -0400

    updated requirements.txt for Coverage

---
+++ chores/tests/models_tests.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import models as auth_models
+from django.contrib import admin
+
+from jsonfield import JSONField
+
+from chores import models
+
