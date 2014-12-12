fc8d426797adbeeaadd93c69ddf84d8fd32f13ea
Willy Collins <wcurtiscollins@gmail.com>
2014-12-11 22:46:33 -0500

    update fab

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,11 @@ from fabric.api import *
 @hosts('applepicke@chores.willyc.me')
 def production():
   code_dir = '/home/applepicke/webapps/chores/chores/'
-  python = '/home/applepicke/webapps/chores/env/default/bin/python'
   with cd(code_dir):
-    run("git pull") 
-    run('bower install') 
-    run('%s manage.py syncdb' % python)
-    run('%s manage.py migrate chores' % python)
-    run('%s /home/applepicke/webapps/chores/chores/manage.py collectstatic --noinput' % python)
-    run('touch chores/wsgi.py')
+    with prefix('. /home/applepicke/webapps/chores/env/default/bin/activate'):
+      run("git pull")
+      run('bower install')
+      run('./manage.py syncdb')
+      run('./manage.py migrate chores')
+      run('./manage.py collectstatic --noinput')
+      run('touch chores/wsgi.py')
