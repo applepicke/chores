edc24edf57c389815e19f96c18afee16eb7e0dd3
Willy Collins <wcurtiscollins@gmail.com>
2015-01-13 20:50:42 -0500

    fixing

--- chores/wsgi.py
+++ chores/wsgi.py
@@ -6,14 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-
 import os, sys, site
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chores.settings")
 
 site.addsitedir('/home/applepicke/.virtualenvs/chores/lib/python2.7/site-packages')
 
-activate_this = os.path.expanduser("~/.virtualenvs/chores/bin/activate_this.py")
-execfile(activate_this, dict(__file__=activate_this))
+from django.conf import settings
+if not settings.DEBUG:
+  activate_this = os.path.expanduser("~/.virtualenvs/chores/bin/activate_this.py")
+  execfile(activate_this, dict(__file__=activate_this))
 
 # Calculate the path based on the location of the WSGI script
 project = '/home/applepicke/webapps/chores/'
