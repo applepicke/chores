5f9d92b9ff37aeac76b1468deb549b8fb731ac8e
Willy Collins <wcurtiscollins@gmail.com>
2015-01-13 21:05:49 -0500

    more changes

--- chores/wsgi.py
+++ chores/wsgi.py
@@ -9,19 +9,5 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 import os, sys, site
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chores.settings")
 
-site.addsitedir('/home/applepicke/.virtualenvs/chores/lib/python2.7/site-packages')
-
-from django.conf import settings
-if not settings.DEBUG:
-  activate_this = os.path.expanduser("~/.virtualenvs/chores/bin/activate_this.py")
-  execfile(activate_this, dict(__file__=activate_this))
-
-# Calculate the path based on the location of the WSGI script
-project = '/home/applepicke/webapps/chores/'
-workspace = os.path.dirname(project)
-sys.path.append(workspace)
-
-sys.path = ['/home/applepicke/webapps/chores/chores', '/home/applepicke/webapps/chores'] + sys.path
-
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
