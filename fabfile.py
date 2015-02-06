0d7556994fbd43a975a6695a993b199fc8875a83
William Collins <william@fluidware.com>
2015-02-06 12:52:12 -0500

    upgrade some fab

--- fabfile.py
+++ fabfile.py
@@ -3,11 +3,18 @@ from fabric.api import *
 @hosts('applepicke@chores.willyc.me')
 def production():
   code_dir = '/home/applepicke/webapps/chores/chores/'
-  with cd(code_dir):
-    with prefix('. /home/applepicke/.virtualenvs/chores/bin/activate'):
+  supervisor_dir = '/home/applepicke/etc/'
+
+  with prefix('. /home/applepicke/.virtualenvs/chores/bin/activate'):
+    with cd(code_dir):
       run("git pull")
       run('bower install')
       run('./manage.py syncdb')
       run('./manage.py migrate chores')
       run('./manage.py collectstatic --noinput')
       run('touch chores/wsgi.py')
+    with cd(supervisor_dir):
+      run('supervisorctl restart chores-celery-tasks')
+      run('supervisorctl restart chores-celery-periodic')
+
+
